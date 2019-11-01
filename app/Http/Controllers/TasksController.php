1d82a1c20d723ab0436c14b661b020612fc0f924
kutaro <tmy_lck@yahoo.co.jp>
2019-11-01 06:50:36 +0000

    validation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -31,7 +31,7 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'status' => 'required|max:10',
-            'content' => 'required|max:10',
+            'content' => 'required|max:191',
         ]);
         
         $task = new Task;
@@ -64,7 +64,7 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'status' => 'required|max:10',
-            'content' => 'required|max:10',
+            'content' => 'required|max:191',
         ]);
         
         $task = Task::find($id);
